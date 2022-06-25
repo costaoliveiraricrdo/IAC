a1cbeb8889b12329db9099c0dd0d18a928c7a1fb
costaoliveiraricrdo <costa.oliveira.ricardo@gmail.com>
2022-06-25 18:59:01 -0300

    Atualizando mainf.tf

--- mainf.tf
+++ mainf.tf
@@ -14,14 +14,21 @@ provider "aws" {
 }
 
 resource "aws_instance" "Web_Server" {
-  ami           = "ami-0b07ee9905d646558"
+  ami           = "ami-052efd3df9dad4825"
   instance_type = "t2.micro"
   key_name      = "Key-RicardoCosta"
+  user_data     = <<-EOF
+                    #!/bin/bash
+                    cd /home/ubuntu
+                    echo "<h1>Terraform Init HTML Page Welcome</h1>" > index.html
+                    nohup busybox httpd -f -p 8080 & 
+                    EOF
   vpc_security_group_ids = [
     "sg-05ab2fb5bca4c0c1d",
-    "sg-01c755d675702f501"
+    "sg-01c755d675702f501",
+    "sg-0677f61f65d157377"
   ]
   tags = {
-    Name = "Web_Server_IaC_01"
+    Name = "Web_Server_IaC_UBU_02"
   }
 }
